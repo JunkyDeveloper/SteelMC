27629c1c36550699af72ee8e17f74b6df990b0ab
ht06 <hthug06@gmail.com>
2026-04-30 14:41:24 +0200

    refactor(command/blacklist-ip): use structs instead of closures

--- steel-core/src/command/commands/blacklist_ip.rs
+++ steel-core/src/command/commands/blacklist_ip.rs
@@ -1,7 +1,9 @@
 //! Handler for the "blacklist-ip" command.
 
 use crate::command::arguments::ip::IpArgument;
-use crate::command::commands::{CommandHandlerBuilder, CommandHandlerDyn, argument, literal};
+use crate::command::commands::{
+    CommandExecutor, CommandHandlerBuilder, CommandHandlerDyn, argument, literal,
+};
 use crate::command::context::CommandContext;
 use crate::command::error::CommandError;
 use crate::network::ip_access_policy::IP_ACCESS_POLICY;
@@ -18,81 +20,89 @@ pub fn command_handler() -> impl CommandHandlerDyn {
         "Add, remove and see blacklisted IPs on the server",
         "minecraft:command.blacklistip",
     )
-    .then(literal("add").then(argument("ip", IpArgument).executes(
-        |((), ip): ((), Option<IpAddr>),
-         context: &mut CommandContext|
-         -> Result<(), CommandError> {
-            // Check if the IP is valid
-            let valid_ip = ip.ok_or_else(|| {
-                CommandError::CommandFailed(Box::new(
-                    TextComponent::plain("This IP is invalid.").color(Color::Red),
-                ))
-            })?;
+    .then(literal("add").then(argument("ip", IpArgument).executes(BlacklistIpExecutor::Add)))
+    .then(literal("remove").then(argument("ip", IpArgument).executes(BlacklistIpExecutor::Remove)))
+    .then(literal("list").executes(BlacklistIpListExecutor))
+}
 
-            // Blacklist the IP
-            if !IP_ACCESS_POLICY.blacklist_ip(&valid_ip) {
-                context.sender.send_message(
-                    &TextComponent::plain("This IP is already blacklisted.").color(Color::Red),
-                );
+/// An enum with the option "Add" and "Remove" from the blacklist-ip command
+enum BlacklistIpExecutor {
+    Add,
+    Remove,
+}
 
-                return Ok(());
-            }
+impl CommandExecutor<((), Option<IpAddr>)> for BlacklistIpExecutor {
+    fn execute(
+        &self,
+        args: ((), Option<IpAddr>),
+        context: &mut CommandContext,
+    ) -> Result<(), CommandError> {
+        let ip = args.1.expect("Invalid IP");
 
-            // Disconnect the player
-            for player in context.server.get_players() {
-                if player.connection.ip_address() == valid_ip {
-                    // Disconnect the player with the right message
-                    player
-                        .connection
-                        .disconnect_with_reason(TextComponent::plain("Disconnected"));
+        match self {
+            BlacklistIpExecutor::Add => {
+                // Blacklist the IP
+                if !IP_ACCESS_POLICY.blacklist_ip(&ip) {
+                    context.sender.send_message(
+                        &TextComponent::plain("This IP is already blacklisted.").color(Color::Red),
+                    );
+                    return Ok(());
                 }
+
+                // Send a message to the sender
+                context.sender.send_message(&TextComponent::plain(format!(
+                    "The IP {ip} is now blacklisted"
+                )));
+
+                // Disconnect the players
+                for player in context.server.get_players() {
+                    if player.connection.ip_address() == ip {
+                        player
+                            .connection
+                            .disconnect_with_reason(TextComponent::plain("Disconnected"));
+                    }
+                }
+
+                Ok(())
             }
+            BlacklistIpExecutor::Remove => {
+                // Check if the IP is blacklisted
+                if !IP_ACCESS_POLICY.is_blacklisted(&ip) {
+                    context.sender.send_message(
+                        &TextComponent::plain("This IP is not blacklisted.").color(Color::Red),
+                    );
+                    return Ok(());
+                }
 
-            // Send a message to the sender
-            context.sender.send_message(&TextComponent::plain(format!(
-                "The IP {valid_ip} is now Blacklisted"
-            )));
-            Ok(())
-        },
-    )))
-    .then(literal("remove").then(argument("ip", IpArgument).executes(
-        |((), ip): ((), Option<IpAddr>),
-         context: &mut CommandContext|
-         -> Result<(), CommandError> {
-            //  Check if the IP is blacklisted
-            if !IP_ACCESS_POLICY.is_blacklisted(&ip.expect("Invalid IP")) {
-                context.sender.send_message(
-                    &TextComponent::plain("This IP is not blacklisted.").color(Color::Red),
-                );
-                return Ok(());
+                // Unblacklist
+                IP_ACCESS_POLICY.un_blacklist_ip(&ip);
+
+                // inform the sender
+                context.sender.send_message(&TextComponent::plain(format!(
+                    "Removed {ip} from the blacklist"
+                )));
+                Ok(())
             }
+        }
+    }
+}
 
-            // Unblacklist
-            IP_ACCESS_POLICY.un_blacklist_ip(&ip.expect("Invalid IP"));
+struct BlacklistIpListExecutor;
 
-            // inform the sender
-            context.sender.send_message(&TextComponent::plain(format!(
-                "Removed {} from the blacklist",
-                ip.expect("Invalid IP")
-            )));
-            Ok(())
-        },
-    )))
-    .then(literal("list").executes(
-        |(): (), context: &mut CommandContext| -> Result<(), CommandError> {
-            // Send the IPs, but sort it before
-            let mut ips = IP_ACCESS_POLICY.get_blacklisted_ips().to_vec();
-            ips.sort();
+impl CommandExecutor<()> for BlacklistIpListExecutor {
+    fn execute(&self, _args: (), context: &mut CommandContext) -> Result<(), CommandError> {
+        // Send the IPs, but sort it before
+        let mut ips = IP_ACCESS_POLICY.get_blacklisted_ips();
+        ips.sort();
 
-            context.sender.send_message(&TextComponent::plain(format!(
-                "Blacklisted IP: {}",
-                ips.iter()
-                    .map(ToString::to_string)
-                    .collect::<Vec<String>>()
-                    .join(", ")
-            )));
+        context.sender.send_message(&TextComponent::plain(format!(
+            "Blacklisted IP: {}",
+            ips.iter()
+                .map(ToString::to_string)
+                .collect::<Vec<String>>()
+                .join(", ")
+        )));
 
-            Ok(())
-        },
-    ))
+        Ok(())
+    }
 }
